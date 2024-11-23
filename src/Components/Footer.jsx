6150007bcc04a641420d50d6466e0401e53898a8
Odgiedev <odgiedev@gmail.com>
2024-11-23 14:30:04 -0300

    Fix background, icon ;

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="bg-black p-5 flex flex-col justify-center items-center">
+        <div className="bg-gray-950 p-5 flex flex-col justify-center items-center">
             <span className="font-bold text-slate-300 text-md">Pensando à Frente</span>
             <span>-</span>
             <span className="font-bold text-slate-300">DIEGO FERNANDES © 2024</span>
